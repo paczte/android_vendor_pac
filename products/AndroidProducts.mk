8b964c0a73ac6bf15733ce8abe00d39e25029e55
Ryan Gay <ryan.sn.karoo@gmail.com>
2013-02-23 13:42:01 +0000

    Add skate and u8815 to products
    Change-Id: Ife9e75eecf38caa2ace85a74c70175b9be8e556d

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -61,3 +61,15 @@ endif
 ifeq (pac_p990,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pac_p990.mk
 endif
+
+
+
+###############
+##zte/huawei###
+###############
+ifeq (pac_skate,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pac_skate.mk
+endif
+ifeq (pac_u8815,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pac_u8815.mk
+endif
