407ab3d2f40bfe2689225be04d19236cff0531c5
Ryan Gay <ryan.sn.karoo@gmail.com>
2013-03-09 15:32:41 +0000

    update pac_skate makefile
    Change-Id: I5b5a9112b92faa9d3eacc4611a8f2e57bb26d0cb

--- products/pac_skate.mk
+++ products/pac_skate.mk
@@ -9,11 +9,11 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/common
 
 # PAC device overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/pac/hdpi_480x800
-#$(shell cp -f vendor/pac/prebuilt/common/bootanimation_framework/android-logo-mask_samsung-mdpi.png frameworks/base/core/res/assets/images/android-logo-mask.png)
+$(shell cp -f vendor/pac/prebuilt/common/bootanimation_framework/android-logo-mask_samsung-mdpi.png frameworks/base/core/res/assets/images/android-logo-mask.png)
 
 # PAC boot logo
-#PRODUCT_COPY_FILES += \
-#    vendor/pac/prebuilt/common/bootlogo/pac_logo_480x800.rle:root/logo.rle
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/common/bootlogo/pac_logo_480x800.rle:root/skate.rle
 
 # include ParanoidAndroid common configuration
 include vendor/pac/config/pac_common.mk
